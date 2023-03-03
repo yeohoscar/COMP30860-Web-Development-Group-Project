d07752df330e1c66f9416cd88ac434cfa53c9d0b
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2023-03-03 19:35:25 +0000

    started adding payments page

---
+++ src/main/java/com/yysw/site/PaymentInformation.java
@@ -0,0 +1,2 @@
+package com.yysw.site;public class PaymentInformation {
+}
--- src/main/java/com/yysw/site/SiteController.java
+++ src/main/java/com/yysw/site/SiteController.java
@@ -1,11 +1,14 @@
 package com.yysw.site;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
 
 @Controller
-public class SiteController {
+public class SiteEGgController {
     @GetMapping
     public String home() {
         return "index.html";
@@ -22,7 +25,21 @@ public class SiteController {
     }
 
     @GetMapping
+    public String register() {
+        return "register.html";
+    }
+
+    @GetMapping
     public String payment() {
         return "payment.html";
+    }
+
+    @PostMapping("/payment")
+    public String submitPayment(@Valid @ModelAttribute("paymentInformation") PaymentInformation paymentInformation, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "payment.html";
+        } else {
+            return "payment_success.html";
+        }
     }
 }
