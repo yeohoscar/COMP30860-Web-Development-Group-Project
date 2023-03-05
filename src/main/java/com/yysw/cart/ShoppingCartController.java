de686a078bd95b63915a95ded1b218cb515d5c8a
rubing-w <rubing.wang@ucdconnect.ie>
2023-03-05 19:49:02 +0000

    add some thymeleaf for listing products, more ai model stuff need to be done before it can be done

--- src/main/java/com/yysw/cart/ShoppingCartController.java
+++ src/main/java/com/yysw/cart/ShoppingCartController.java
@@ -2,12 +2,33 @@ package com.yysw.cart;
 
 import com.yysw.site.AiModel;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
 import java.util.ArrayList;
 
 @Controller
 public class ShoppingCartController {
     ArrayList<AiModel> modelsInCart = new ArrayList<>();
+
+    //need to add the other shoppingcart's getMapping when merging branches
+    @GetMapping("/ShoppingCart")
+    public String shoppingCart(Model model) {
+        model.addAttribute("size",modelsInCart.size());
+        model.addAttribute("products",modelsInCart);
+        double sub=0.0;
+        double processfee=200;
+        //TODO: add price together
+//        for (AiModel a:modelsInCart) {
+//            sub += a.price();
+//        }
+        model.addAttribute("subtotal", sub);
+        //TODO: this is just a random value, change later
+        model.addAttribute("fee", processfee);
+        model.addAttribute("discount", sub*0.2);
+        model.addAttribute("tot",sub+processfee-(sub*0.2));
+        return "shoppingCart.html";
+    }
 
 
 }
