3ef88363979e172b91fe9556583e2572bbf2d76e
rubing-w <rubing.wang@ucdconnect.ie>
2023-03-05 19:17:36 +0000

    html for shopping cart is done, css style need to be moved into style.css file later, haven't added any thymeleaf yet

---
+++ src/main/java/com/yysw/cart/ShoppingCartController.java
@@ -0,0 +1,13 @@
+package com.yysw.cart;
+
+import com.yysw.site.AiModel;
+import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+
+@Controller
+public class ShoppingCartController {
+    ArrayList<AiModel> modelsInCart = new ArrayList<>();
+
+
+}
