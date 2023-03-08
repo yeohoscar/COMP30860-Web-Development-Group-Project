3a1201c7614a4518d24128e14f10c1d5bcf2b228
YanhaoSun <sunyanhao134@gmail.com>
2023-03-08 23:47:03 +0000

    update orderView HTML

--- src/main/java/com/yysw/orderHistory/OrderHistoryController.java
+++ src/main/java/com/yysw/orderHistory/OrderHistoryController.java
@@ -14,35 +14,35 @@ public class OrderHistoryController {
     HashMap<Integer, Order> myOrders = new HashMap<>();
 
    //DO NOT DELETE THIS METHOD!!!
-//    @GetMapping("/orderHistory")
-//    public String orderHistory(Model model)
-//    {
-//        model.addAttribute("orders", myOrders.values());
-//        return "orderHistory.html";
-//    }
+    @GetMapping("/orderHistory")
+    public String orderHistory(Model model)
+    {
+        model.addAttribute("orders", myOrders.values());
+        return "orderHistory.html";
+    }
 
     //this one is the one with specific order ID, DO NOT DELETE!!!
-//    @GetMapping("/viewOrder/{id}")
-//    public String viewOrder(Model model, @PathVariable int id)
-//    {
-//        model.addAttribute("view", myOrders.get(id));
-//        return "viewOrder.html";
-//    }
+    @GetMapping("/viewOrder/{id}")
+    public String viewOrder(Model model, @PathVariable int id)
+    {
+        model.addAttribute("view", myOrders.get(id));
+        return "viewOrder.html";
+    }
 
 
 
     //just for testing viewOrder, deletable
-    @GetMapping("/orderHistory")
-    public String orderHistory()
-    {
-        return "orderHistory.html";
-    }
-
-    @GetMapping("/viewOrder")
-    public String viewOrder()
-    {
-        return "viewOrder.html";
-    }
+//    @GetMapping("/orderHistory")
+//    public String orderHistory()
+//    {
+//        return "orderHistory.html";
+//    }
+//
+//    @GetMapping("/viewOrder")
+//    public String viewOrder()
+//    {
+//        return "viewOrder.html";
+//    }
 
 
 }
