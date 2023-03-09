140971e5dd2b569655b149db6b89fccfa6d9930b
YanhaoSun <sunyanhao134@gmail.com>
2023-03-09 12:03:01 +0000

    update orderView HTML

--- src/main/java/com/yysw/orderHistory/OrderHistoryController.java
+++ src/main/java/com/yysw/orderHistory/OrderHistoryController.java
@@ -1,6 +1,7 @@
 package com.yysw.orderHistory;
 
 import com.yysw.order.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +11,9 @@ import java.util.HashMap;
 
 @Controller
 public class OrderHistoryController {
+    @Autowired
+    private OrderRepository orderRepository;
+    List<Order> orders = orderRepository.findByCustomerOrderByOrderDateDesc(Customer customer);
     //assume customers' orders are stored in the HashMap below
     HashMap<Integer, Order> myOrders = new HashMap<>();
 
