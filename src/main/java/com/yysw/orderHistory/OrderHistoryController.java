80a04815edfb6970ba3fd35b8e103f51c1205b54
YanhaoSun <sunyanhao134@gmail.com>
2023-03-09 13:15:44 +0000

    ready to take input from customers

--- src/main/java/com/yysw/orderHistory/OrderHistoryController.java
+++ src/main/java/com/yysw/orderHistory/OrderHistoryController.java
@@ -1,27 +1,30 @@
 package com.yysw.orderHistory;
 
 import com.yysw.order.Order;
+import com.yysw.order.OrderRepository;
+import com.yysw.user.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
-public class OrderHistoryController {
+public class OrderHistoryController{
     @Autowired
     private OrderRepository orderRepository;
-    List<Order> orders = orderRepository.findByCustomerOrderByOrderDateDesc(Customer customer);
-    //assume customers' orders are stored in the HashMap below
-    HashMap<Integer, Order> myOrders = new HashMap<>();
+    List<Order> orders = new ArrayList<>();
 
    //DO NOT DELETE THIS METHOD!!!
     @GetMapping("/orderHistory")
-    public String orderHistory(Model model)
+    public String orderHistory(Customer customer, Model model)
     {
-        model.addAttribute("orders", myOrders.values());
+        orders = orderRepository.findByCustomerOrderByOrderDateDesc(customer);
+        model.addAttribute("orders", orders);
         return "orderHistory.html";
     }
 
@@ -29,24 +32,8 @@ public class OrderHistoryController {
     @GetMapping("/viewOrder/{id}")
     public String viewOrder(Model model, @PathVariable int id)
     {
-        model.addAttribute("view", myOrders.get(id));
+        model.addAttribute("view", orders.get(id));
         return "viewOrder.html";
     }
-
-
-
-    //just for testing viewOrder, deletable
-//    @GetMapping("/orderHistory")
-//    public String orderHistory()
-//    {
-//        return "orderHistory.html";
-//    }
-//
-//    @GetMapping("/viewOrder")
-//    public String viewOrder()
-//    {
-//        return "viewOrder.html";
-//    }
-
 
 }
