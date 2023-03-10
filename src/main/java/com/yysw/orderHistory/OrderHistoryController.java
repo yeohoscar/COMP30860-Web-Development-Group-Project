956b68c4603a693aeb7f0c221ff5b16a9c0e3fa8
YanhaoSun <sunyanhao134@gmail.com>
2023-03-10 22:34:15 +0000

    OrderHistoryController, OrderHistory and viewOrder are updated and works successfully

--- src/main/java/com/yysw/orderHistory/OrderHistoryController.java
+++ src/main/java/com/yysw/orderHistory/OrderHistoryController.java
@@ -2,82 +2,34 @@ package com.yysw.orderHistory;
 
 import com.yysw.order.Order;
 import com.yysw.order.OrderRepository;
-import com.yysw.order.OrderedModel;
 import com.yysw.user.customer.Customer;
-import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import java.sql.Connection;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 public class OrderHistoryController{
     @Autowired
     private OrderRepository orderRepository;
     List<Order> orders = new ArrayList<>();
-    HashMap<Order, Double> item = new HashMap<>();
-    HashMap<Long, Order> ITEM = new HashMap<>();
-
-
-
-//    @GetMapping("/orderHistory")
-//    public String orderHistory(Customer customer, Model model)
-//    {
-//        double totalPrice= 0.0;
-//        orders = orderRepository.findByCustomer(customer);
-//        for(Order a: orders)
-//        {
-//            ITEM.put(a.getOrder_id(), a);
-//
-//        }
-//
-//        model.addAttribute("customerOrders", ITEM.entrySet());
-//
-//        return "orderHistory.html";
-//    }
-
-
-    //this one is the one with specific order ID, DO NOT DELETE!!!
-//    @GetMapping("/viewOrder/{id}")
-//    public String viewOrder(Model model, @PathVariable Long id)
-//    {
-//        System.out.println("Successful"+id);
-////        HashMap<Order, Double> specificOrder = new HashMap<>();
-////        specificOrder.put(order, item.get(order));
-////        model.addAttribute("view", specificOrder.entrySet());
-//        model.addAttribute("view", ITEM.get(id));
-//
-//        return "viewOrder.html";
-//    }
-
+    HashMap<Long, Order> item = new HashMap<>();
 
     //DO NOT DELETE THIS METHOD!!!
     @GetMapping("/orderHistory")
     public String orderHistory(Customer customer, Model model)
     {
-        double totalPrice= 0.0;
         orders = orderRepository.findByCustomer(customer);
         for(Order a: orders)
         {
-            for(OrderedModel m: a.getOrderedModels())
-            {
-                totalPrice += m.getPrice();
-            }
-            item.put(a, totalPrice);
+            item.put(a.getOrder_id(), a);
         }
-        for(Map.Entry<Order, Double> a: item.entrySet())
-        {
-            System.out.println("key = "+a.getKey().getOrder_id());
-        }
-        model.addAttribute("customerOrders", item.entrySet());
-
+        model.addAttribute("customerOrders", orders);
         return "orderHistory.html";
     }
 
@@ -86,23 +38,7 @@ public class OrderHistoryController{
     public String viewOrder(Model model, @PathVariable Long id)
     {
         System.out.println("Successful"+id);
-        HashMap<Order, Double> specificOrder = new HashMap<>();
-        for(Map.Entry<Order, Double> a: item.entrySet())
-        {
-            if(a.getKey().getOrder_id()==id)
-            {
-                specificOrder.put(a.getKey(), a.getValue());
-            }
-            System.out.println("key = "+a.getKey().getOrder_id());
-        }
-        model.addAttribute("views", specificOrder.entrySet());
-
+        model.addAttribute("view", item.get(id));
         return "viewOrder.html";
     }
-
-
-
-
-
-
 }
