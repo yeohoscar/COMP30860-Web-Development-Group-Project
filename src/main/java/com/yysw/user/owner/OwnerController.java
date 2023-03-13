8062b9afd3d7b4cdf6287fc90ec0bbe7ee9f970c
Oscar Yeoh <oscar.yeoh@ucdconnect.ie>
2023-03-13 02:28:34 +0000

    added implementation thought for owner controler

--- src/main/java/com/yysw/user/owner/OwnerController.java
+++ src/main/java/com/yysw/user/owner/OwnerController.java
@@ -16,21 +16,33 @@ public class OwnerController {
     @Autowired
     AIModelRepository aiModelRepository;
 
-    @Autowired
-    AIModelRepository aiModelRepository;
-
+    /**
+     * TODO: add html page
+     * prolly something like a form that has all the fields
+     * redirects back to itself so you can add more
+     * way to exit back to home page or marketplce
+     */
     @GetMapping("add-model")
     public String addModel(Model model) {
         model.addAttribute("newModel", new AIModel());
         return("add-model.html");
     }
 
+    /**
+     * TODO: Some checks are necessary for new model
+     * Either through validation in this method or like PaymentInformation using javax
+     */
     @PostMapping("add-model")
     public String addModel(@ModelAttribute("newModel") AIModel aiModel) {
         aiModelRepository.save(aiModel);
         return "add-model";
     }
 
+    /**
+     * TODO: not too sure about page
+     * Most simple is to display all models, and have a checkbox that toggles
+     * AIModel available field.
+     */
     @GetMapping("toggle-models")
     public String toggleModels(Model model) {
         model.addAttribute("models", aiModelRepository.findAll());
@@ -41,10 +53,5 @@ public class OwnerController {
     public String toggleModels(@ModelAttribute("models") List<AIModel> models) {
         aiModelRepository.saveAll(models);
         return "toggle-models.html";
-    }
-
-    @GetMapping("view-orders")
-    public String viewOrders(Model model) {
-
     }
 }
