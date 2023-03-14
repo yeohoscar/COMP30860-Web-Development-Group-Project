6d9e473ca1c9668bcda438b9ac9a004950fa6308
kar-yen <kar.yap@ucdconnect.ie>
2023-03-14 18:54:38 +0000

    change state of order

--- src/main/java/com/yysw/order/OrderHistoryController.java
+++ src/main/java/com/yysw/order/OrderHistoryController.java
@@ -1,14 +1,17 @@
 package com.yysw.order;
 
+import com.yysw.user.User;
 import com.yysw.user.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -18,16 +21,19 @@ public class OrderHistoryController{
 
     //DO NOT DELETE THIS METHOD!!!
     @GetMapping("/order-history")
-    public String orderHistory(Customer customer, Model model) {
+    public String orderHistory(Customer customer, ModelMap modelMap, HttpServletRequest request) {
+        User sessionUser = (User) request.getSession().getAttribute("username");
+        // session user wont be null because order history can only access by user after login
         List<Order> orders;
         // TODO: CHECK IF USER IS OWNER OR CUSTOMER
-        if (false) {
+        if (sessionUser instanceof Customer) {
             orders = orderRepository.findByCustomerOrderByOrderDateDesc(customer);
         } else {
             orders = orderRepository.findAllByOrderByOrderDateDescStateAsc();
         }
 
-        model.addAttribute("orders", orders);
+        modelMap.addAttribute("orders", orders);
+        modelMap.addAttribute("user", sessionUser);
         return "order-history.html";
     }
 
