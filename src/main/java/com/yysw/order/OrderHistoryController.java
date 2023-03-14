202e9de01214f41ad554359911f9f0e826a6f537
kar-yen <kar.yap@ucdconnect.ie>
2023-03-14 17:44:06 +0000

    conflict resolved

--- src/main/java/com/yysw/order/OrderHistoryController.java
+++ src/main/java/com/yysw/order/OrderHistoryController.java
@@ -1,34 +1,20 @@
 package com.yysw.order;
 
-<<<<<<< HEAD
-import com.yysw.aimodels.AIModel;
-=======
->>>>>>> main
-import com.yysw.order.Order;
-import com.yysw.order.OrderRepository;
 import com.yysw.user.customer.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-=======
-import org.springframework.web.bind.annotation.PathVariable;
->>>>>>> main
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 @Controller
 public class OrderHistoryController{
     @Autowired
     private OrderRepository orderRepository;
-<<<<<<< HEAD
-    HashMap<Long, Order> item = new HashMap<>();
 
     //DO NOT DELETE THIS METHOD!!!
     @GetMapping("/order-history")
